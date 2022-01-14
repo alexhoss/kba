f7ea503cddc34c424a58128b5313cb8e08652318
alex h <lexhosseini@gmail.com>
2022-01-13 16:12:15 -0800

    functional to some degree

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,80 +1,95 @@
-const postModel = require('../models/postData');
-const userData = require('../models/userData')
-const messageData = require('../models/messageData')
+const postModel = require("../models/postData");
+const userData = require("../models/userData");
+const messageData = require("../models/messageData");
 
 exports.getAllPosts = (req, res, next) => {
-    let Posts = postModel.getAllPosts();
-    Posts.then(([rows, fieldData]) => {
-        let allPostData = rows
-        console.log(localStorage.getItem('id'));
-        let id = localStorage.getItem('id')
-        let postNumber = 0
-        postModel.getNumberOfRowsByID(id).then(data =>{
-            postNumber = data[0][0].num
-        })
-        let messageNumber = 0
-        messageData.getNumberOfRecievedMSGByID(id).then(data =>{
-            messageNumber = data[0][0].num
-        })
-
-        userData.getUserByID(id).then(([rows, fieldData])=>{
-            console.log(rows)
-            let userData = rows
-            res.render('all_posts', {people: rows, peoplesCSS: true, curtisCSS:true, userData: userData[0], people: allPostData, postNumber: postNumber, messageNumber: messageNumber});
-        })        
+  let Posts = postModel.getAllPosts();
+  Posts.then(([rows, fieldData]) => {
+    let allPostData = rows;
+    console.log(localStorage.getItem("id"));
+    let id = localStorage.getItem("id");
+    let postNumber = 0;
+    postModel.getNumberOfRowsByID(id).then((data) => {
+      postNumber = data[0][0].num;
     });
-};//sdsdsd
+    let messageNumber = 0;
+    messageData.getNumberOfRecievedMSGByID(id).then((data) => {
+      messageNumber = data[0][0].num;
+    });
+
+    userData.getUserByID(id).then(([rows, fieldData]) => {
+      console.log(rows);
+      let userData = rows;
+      res.render("all_posts", {
+        people: rows,
+        peoplesCSS: true,
+        curtisCSS: true,
+        userData: userData[0],
+        people: allPostData,
+        postNumber: postNumber,
+        messageNumber: messageNumber,
+      });
+    });
+  });
+}; //sdsdsd
 
 exports.getAllPostsHome = (req, res, next) => {
-    let Posts = postModel.getAllPosts();
-    Posts.then(([rows, fieldData]) => {
-        let allPostData = rows
-        console.log(localStorage.getItem('id'));
-        let id = localStorage.getItem('id')
-        let postNumber = 0
-        postModel.getNumberOfRowsByID(id).then(data =>{
-            postNumber = data[0][0].num
-        })
-        let messageNumber = 0
-        messageData.getNumberOfRecievedMSGByID(id).then(data =>{
-            messageNumber = data[0][0].num
-        })
-        userData.getUserByID(id).then(([rows, fieldData])=>{
-            console.log(rows)
-            let userData = rows
-            res.render('home_johnny', {people: rows, peoplesCSS: true, curtisCSS:true, userData: userData[0], people: allPostData, postNumber: postNumber, messageNumber: messageNumber});
-        })        
+  let Posts = postModel.getAllPosts();
+  Posts.then(([rows, fieldData]) => {
+    let allPostData = rows;
+    console.log(localStorage.getItem("id"));
+    let id = localStorage.getItem("id");
+    let postNumber = 0;
+    postModel.getNumberOfRowsByID(id).then((data) => {
+      postNumber = data[0][0].num;
     });
+    let messageNumber = 0;
+    messageData.getNumberOfRecievedMSGByID(id).then((data) => {
+      messageNumber = data[0][0].num;
+    });
+    userData.getUserByID(id).then(([rows, fieldData]) => {
+      console.log(rows);
+      let userData = rows;
+      res.render("home_johnny", {
+        people: rows,
+        peoplesCSS: true,
+        curtisCSS: true,
+        userData: userData[0],
+        people: allPostData,
+        postNumber: postNumber,
+        messageNumber: messageNumber,
+      });
+    });
+  });
 };
 
 exports.postDiscussionRequest = (req, res, next) => {
-    let p_subject = req.body.subject;
-    let p_topic = req.body.topic;
-    let p_desc = req.body.explain;
-    let p_author = localStorage.getItem('id');
-    let pOject = {
-        subject: p_subject,
-        about: p_desc,
-        topic: p_topic,
-        author: p_author
-    }
+  let p_subject = req.body.subject;
+  let p_topic = req.body.topic;
+  let p_desc = req.body.explain;
+  let p_author = localStorage.getItem("id");
+  let pOject = {
+    subject: p_subject,
+    about: p_desc,
+    topic: p_topic,
+    author: p_author,
+  };
 
-    postModel.add(pOject);
-    res.redirect(301, '/home');
-}
+  postModel.add(pOject);
+  res.redirect(301, "/home");
+};
 
 exports.postReplyRequest = (req, res, next) => {
-    let p_subject = req.body.replyText;
-    let p_topic = req.body.postID;
-    console.log(req.body)
+  let p_subject = req.body.replyText;
+  let p_topic = req.body.postID;
+  console.log(req.body);
 
-    let pOject = {
-        desc: p_subject,
-        author: localStorage.getItem('id'),
-        topic: p_topic
-    }
+  let pOject = {
+    desc: p_subject,
+    author: localStorage.getItem("id"),
+    topic: p_topic,
+  };
 
-
-    postModel.addReply(pOject);
-    res.redirect(301, '/all_posts');
-}
+  postModel.addReply(pOject);
+  res.redirect(301, "/all_posts");
+};
--- models/postData.js
+++ models/postData.js
@@ -1,47 +1,65 @@
-let db = require('../util/database');
+let db = require("../util/database");
 // Gets all the individuals in the database
 // function getTop5Post() {
 //     let sql = "Select * from posts ORDER BY date DESC LIMIT 5"
-//     return db.execute(sql)   
+//     return db.execute(sql)
 // }
 // Add a single individual to the database
 function addPost(data) {
-   
-    let sql = "Insert into posts (title, topic, topic_desc, author_id) values ('" + data.subject + "','" + data.topic + "','" + data.about + "','" + data.author + "')";
-    return db.execute(sql);
+  let sql =
+    "Insert into posts (title, topic, topic_desc, author_id) values ('" +
+    data.subject +
+    "','" +
+    data.topic +
+    "','" +
+    data.about +
+    "','" +
+    data.author +
+    "')";
+  return db.execute(sql);
 }
 function getAll(data) {
-   let  sql =  "Select * from posts LEFT JOIN users ON posts.author_id = users.id LEFT JOIN replies ON posts.pk_post_id = replies.post_id ORDER BY posts.date DESC";
-   return db.execute(sql);
+  let sql =
+    "Select * from posts LEFT JOIN users ON posts.author_id = users.id LEFT JOIN replies ON posts.pk_post_id = replies.post_id ORDER BY posts.date DESC";
+  return db.execute(sql);
 }
 function getAuthorPic(id) {
-    let sql = "Select img_url from users where id = " + id;
-    console.log 
+  let sql = "Select img_url from users where id = " + id;
+  return db.execute(sql);
 }
 
-function getReplies(data){
-    let sql = r
+function getReplies(post_id) {
+  let sql = "Select reply from users where id = " + post_id;
+  return db.execute(sql);
 }
 
-function addReplyDB(data){
-    let sql = "Insert into replies (post_id, author_id, post_desc) values ('" + data.topic + "','" + data.author + "','" + data.desc + "')";
-    return db.execute(sql);
+function addReplyDB(data) {
+  let sql =
+    "Insert into replies (post_id, author_id, post_desc) values ('" +
+    data.topic +
+    "','" +
+    data.author +
+    "','" +
+    data.desc +
+    "')";
+  return db.execute(sql);
 }
 function getAllbyKeyword(keyword) {
-    let combinedTable = "Select * from posts LEFT JOIN users ON posts.author_id = users.id LEFT JOIN replies ON posts.pk_post_id = replies.post_id"
-    let condition = `(posts.title LIKE '%${keyword}%' OR posts.topic LIKE '%${keyword}%' OR posts.topic_desc LIKE '%${keyword}%' OR users.name LIKE '%${keyword}%' OR replies.post_desc LIKE '%${keyword}%')`
-    let sql = `${combinedTable} WHERE ${condition}`
-    return db.execute(sql)
+  let combinedTable =
+    "Select * from posts LEFT JOIN users ON posts.author_id = users.id LEFT JOIN replies ON posts.pk_post_id = replies.post_id";
+  let condition = `(posts.title LIKE '%${keyword}%' OR posts.topic LIKE '%${keyword}%' OR posts.topic_desc LIKE '%${keyword}%' OR users.name LIKE '%${keyword}%' OR replies.post_desc LIKE '%${keyword}%')`;
+  let sql = `${combinedTable} WHERE ${condition}`;
+  return db.execute(sql);
 }
 
-function getNumberOfRowsByID(id){
-    let sql = `Select Count(*) as num From posts Where author_id = ${id}`
-    return db.execute(sql)
+function getNumberOfRowsByID(id) {
+  let sql = `Select Count(*) as num From posts Where author_id = ${id}`;
+  return db.execute(sql);
 }
 module.exports = {
-    add: addPost,
-    getAllPosts: getAll,
-    addReply: addReplyDB,
-    getAllbyKeyword: getAllbyKeyword,
-    getNumberOfRowsByID: getNumberOfRowsByID
-}
+  add: addPost,
+  getAllPosts: getAll,
+  addReply: addReplyDB,
+  getAllbyKeyword: getAllbyKeyword,
+  getNumberOfRowsByID: getNumberOfRowsByID,
+};
